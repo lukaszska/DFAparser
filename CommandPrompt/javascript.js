6f3de9fb66f2d8abed923e11f6957fc4e301ea47
Ahmad Dinkins <ahmadid2@illinois.edu>
2018-10-05 10:25:36 -0500

    The outline for the Command Prompt

---
+++ CommandPrompt/javascript.js
@@ -0,0 +1,31 @@
+document.onkeydown = checkKey;
+
+function checkKey(e){
+    e = e || window.event;
+
+    if(e.keyCode == '13' && (document.activeElement === document.getElementById("command-input"))){
+        console.log("enter key pressed");
+        enterCommand();
+    }
+}
+
+function enterCommand(){
+    var text = document.getElementById("command-input").value;
+    setConsole(text);
+    //runCommand(text);
+    document.getElementById("command-input").value = "";
+}
+
+function setConsole(text){
+    document.getElementById("console").innerHTML = document.getElementById("console").innerHTML + text + "<br>";
+    document.getElementById("console").scrollTop = document.getElementById("console").scrollHeight;
+}
+
+function getText() {
+    return document.getElementById("console").innerHTML;
+}
+
+function setImage(){
+    document.getElementById("image").src = "img/pepehands.png";
+    console.log(getText());
+}
