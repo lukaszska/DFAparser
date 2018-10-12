583e8ef4639f9e83e7ce6042b6a663837aeeff4c
kevin <kevin88tu@gmaillcom>
2018-10-12 15:51:42 -0500

    Added navbar and some demo animations

---
+++ javascript.js
@@ -0,0 +1,44 @@
+document.onkeydown = checkKey;
+
+function checkKey(e){
+    e = e || window.event;
+
+    if(e.keyCode == '13' && (document.activeElement === document.getElementById("command-input"))){
+        //console.log("enter key pressed");
+        enterCommand();
+    }
+}
+
+function enterCommand(){
+    var text = document.getElementById("command-input").value;
+    setConsole(text);
+    //runCommand(text);
+    document.getElementById("command-input").value = "";
+}
+
+function setConsole(text){
+    document.getElementById("console").innerHTML = document.getElementById("console").innerHTML + text + "<br>";
+    document.getElementById("console").scrollTop = document.getElementById("console").scrollHeight;
+    readInput(text);
+}
+
+function getText() {
+    return document.getElementById("console").innerHTML;
+}
+
+function setImage(){
+    document.getElementById("image").src = "pepehands.png";
+    //console.log(getText());
+}
+
+function readInput(text) {
+    if (text === "left to right") {
+        leftToRight();
+    } else if (text === "right to left") {
+        rightToLeft();
+    } else if (text === "left to left") {
+        leftToLeft();
+    } else if (text === "right to right") {
+        rightToRight();
+    }
+}
