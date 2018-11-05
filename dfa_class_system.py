3744315ff16b79b65cebef502ad4c543741984d1
adamac2 <adamac2@illinois.edu>
2018-11-05 12:29:01 -0600

    DFA is "functional"

--- dfa_class_system.py
+++ dfa_class_system.py
@@ -5,6 +5,15 @@ BUGS TO FIX:
 - remove transition crashes for nodes that don't exist
 - remove transition needs the transition key and any node to delete, should only need one or the other
 - assumes that transitions parameter fits nicely in 3s
+- doesn't check if key is in alphabet
+- assumes every node has a transition for each letter in alphabet and no dead ends
+
+GOALS:
+- get it to use the regex script
+- create command line interface
+- combind add/remove methods into one method, or make separate function that decides which to call
+- function needs to read beginning flag of tuple and call appropriate function
+- clean tuple in flag reading function, and send a list to DFA creation methods
 """
 class DFA:
     # Create the data structure that will represent the DFA
@@ -64,16 +73,23 @@ class DFA:
                 self.alphabet.append(a_list[i])
 
     def check_string(self, s):
+        current_node = self.start
         for c in s:
-            self.start = self.dfa_dict[self.start][c]
-        return self.start in self.accepted_states
+            print(current_node)
+            print(self.dfa_dict[current_node][c])
+            current_node = self.dfa_dict[current_node][c]
+        return current_node in self.accepted_states
 
+# TESTING THE DFA, note first part of tuple doesn't have a use atm
 meme = DFA()
-meme.add_nodes(('0', 'a', 'b', 'c'))
+meme.add_nodes(('memes', 'a', 'b', 'c'))
 print(meme.dfa_dict)
-meme.add_transitions(('5', 'a', 'b', '0', 'b', 'c', '0', 'c', 'c', '0', 'a', 'a', '1', 'b', 'a', '1', 'c', 'c', '1'))
+meme.add_transitions(('bazinga!', 'a', 'b', '0', 'b', 'c', '0', 'c', 'c', '0', 'a', 'a', '1', 'b', 'a', '1', 'c', 'c', '1'))
 print(meme.dfa_dict)
-meme.set_alphabet(('yikes', '0', '1'))
+meme.set_alphabet(('y i k e s', '0', '1'))
 print(meme.alphabet)
-meme.set_accepts(('0', 'c'))
+meme.set_accepts(('Fortnite > PUBG', 'c'))
+meme.add_starting_node(('SPOILER: spiderman dies in infinity war', 'a'))
+print(meme.accepted_states)
+print(meme.check_string("000"))
     
